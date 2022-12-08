80d703c7cc8a61a0153e6feff6231a3b7d3fa958
Lenilk <71206634+Lenilk@users.noreply.github.com>
2022-12-08 08:21:41 +0700

    first my function

---
+++ function/1.py
@@ -0,0 +1,10 @@
+def money(day,hr,work):
+    if (day.lower()).title()=="Sunday":
+        return hr*(3*work)
+    else:
+        return hr*((3/2)*work)
+print("โปรแกรมคำนวณค่าแรง")
+print("day <space> hour <space> normalwage")
+print("**day: Sunday Monday Tuesday wednesday Thursday Friday Saturnday**")
+day,hr,work=[str(x) for x in input().split()]
+print(money(day,int(hr),int(work)))
